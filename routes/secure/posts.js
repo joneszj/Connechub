5f2105238a7f0ecb51f1e1096d92fee5d8b000ca
Zachary <joneszj@gmail.com>
2017-08-02 18:32:39 -0400

    bump

--- routes/secure/posts.js
+++ routes/secure/posts.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 // update profile post
-router.post('/profile/:id/posts/:id', function (req, res, next) {
+router.post('/profile/:id/posts/:id', passport.authenticate('bearer', { session: false }), function (req, res, next) {
 
 });
 
